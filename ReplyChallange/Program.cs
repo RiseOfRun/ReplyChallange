9adc3d62309f9a01aeed72d3d385ef232a68b86e
RiseOfRun <runi54rus@gmail.com>
2020-03-13 00:11:41 +0700

    parser map

--- ReplyChallange/Program.cs
+++ ReplyChallange/Program.cs
@@ -1,9 +1,56 @@
-﻿namespace ReplyChallange
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReplyChallange
 {
+    abstract class Unit
+    {
+        public string Company;
+        public int Bonus;
+        public int[] coords = new int[2];
+        public abstract int ComputeTP(Unit neighbour);
+    }
+
+    class Developer : Unit
+    {
+        List<string> Skills = new List<string>();
+        public override int ComputeTP(Unit neighbour)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class ProjectManager : Unit
+    {
+        public override int ComputeTP(Unit neighbour)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
     internal class Program
     {
+        
         public static void Main(string[] args)
         {
+            int width, height;
+            List<Unit> units= new List<Unit>();
+            //{i,j}
+            string[] lines = File.ReadAllLines("a_solar.txt");
+            width = int.Parse(lines[0][0].ToString());
+            height = int.Parse(lines[0][2].ToString());
+            KeyValuePair<char,bool>[,] map =new KeyValuePair<char, bool>[width,height];
+
+            for (int i = 0; i < height; i++)
+            {
+                int j = 0;
+                foreach (var node in lines[i + 1])
+                {
+                    map[i,j] = new KeyValuePair<char, bool>(node,false);
+                }
+            }
+            
         }
     }
 }
